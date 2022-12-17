498a01175eb43fbc5d2d72f891028cabc3bc130a
Roman Tarnakin <roman_tarnakin@epam.com>
2022-12-17 11:09:01 +0300

    add some useEffects

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // the naive approach for checking localStorage and updating component state based on what's inside localStorage
+  // is not going to work as we probably expect because this code will run in an INDEFINITE loop:
+  // get value from localStorage -> "true" -> call 'setIsLoggedIn' -> component state update scheduled -> this code run again.
+  // So basically we need a way of calling this ONLY when we need it and not on every component rerender.
+  // const storedLoggingValue = localStorage.getItem("isLoggedIn");
+  // if (storedLoggingValue === "true") {
+  //   setIsLoggedIn(true);
+  // }
+
+  useEffect(() => {
+    // useEffect is basically an ADVANCED IF CONDITION TIED TO DEPENDENCIES ARRAY
+    // we don't wanna run it on every rerender/reevaluation of component but only when we RESTART the app (that's the whole point
+    // of using localStorage to make login information persist between application runs) - that's why dependencies array is EMPTY
+    const storedLoggingValue = localStorage.getItem("isLoggedIn");
+
+    if (storedLoggingValue === "true") {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "true");
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
   };
 
   return (
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    setEmailIsValid(enteredEmail.includes("@"));
   };
 
   const validatePasswordHandler = () => {
@@ -40,12 +32,40 @@ const Login = (props) => {
     props.onLogin(enteredEmail, enteredPassword);
   };
 
+  useEffect(
+    () => {
+      // as we don't wanna check form validity on EVERY keystroke we can make a simple debounce with the help of setTimeout function:
+      const timer = setTimeout(() => {
+        setFormIsValid(
+          enteredPassword.trim().length > 6 && enteredEmail.includes("@")
+        );
+      }, 500);
+
+      // this is a cleanup function which is executed BEFORE:
+      // - next useEffect call (next rerender);
+      // - component unmounts (removed from the DOM);
+      // IMPORTANT: cleanup function IS NOT EXECUTED before FIRST useEffect call (when component mounts).
+      // so basically it helps us REMOVE the timer which was set on the previous keystroke if we make a new keystroke.
+      // why do we need this? because it doesn't make any sense to check form validity on the OLD input (previous keystroke)
+      // and only makes sense to check it on the latest keystroke!
+      return () => clearTimeout(timer);
+    },
+    // About adding dependencies to the dependencies array:
+    // basically we need to add everything that
+    // - is USED inside useEffect;
+    // - and could change on render/rerender of the component/parent components (state, props)
+    // So it means that NOT EVERYTHING inside useEffect should be added to dependencies array. Exceptions are:
+    // - variables defined OUTSIDE component (either our own defined variables or some external APIs variables);
+    // - useState setter functions (React makes sure that they are the same between renders);
+    [enteredEmail, enteredPassword]
+  );
+
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
@@ -59,7 +79,7 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
